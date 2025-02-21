2d70917f5f0370553663d22faee55af9155cedbd
DivyanshD07 <devdhyani34@gmail.com>
2025-02-22 02:23:03 +0530

    zustand for state management

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUserForSidebar } from "../controllers/message.controller.js";
+import { getMessages, getUserForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.use(protectRoute);
 router.get("/user", getUserForSidebar);
 router.get("/messages/:id", getMessages);
+router.post("/messages/send/:id", sendMessage);
 
 export default router;
